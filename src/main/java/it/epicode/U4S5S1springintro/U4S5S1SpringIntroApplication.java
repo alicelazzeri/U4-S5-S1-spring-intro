1346cb44ad667ad35e29c14f0a1196fdaaf70546
Alice Lazzeri <alice.lazzeri@hotmail.it>
2024-05-13 12:24:45 +0200

    Initial commit

---
+++ src/main/java/it/epicode/U4S5S1springintro/U4S5S1SpringIntroApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.U4S5S1springintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U4S5S1SpringIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U4S5S1SpringIntroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/U4S5S1springintro/U4S5S1SpringIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.U4S5S1springintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U4S5S1SpringIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
